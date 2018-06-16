59ff71ab164eebb66c4cab05705e5abe0ce5ffed
D Tim Cummings <tim@triptera.com.au>
2018-06-16 10:25:55 +1000

    Move testing to PyCharm (from Minetest builder_police) for faster testing.

--- set2/pycharm-edu-3/Minetest 1 - Building/Chequered square/tests.py
+++ set2/pycharm-edu-3/Minetest 1 - Building/Chequered square/tests.py
@@ -3,13 +3,21 @@
 # Authorised for use by schools and CoderDojo in 2018
 
 from test_helper import run_common_tests, failed, passed, get_answer_placeholders
-from ircbuilder import MinetestConnection, NICK_MAX_LEN
+from ircbuilder import MinetestConnection, NICK_MAX_LEN, nodebuilder
 from minetest_irc import ircserver, mtuser, mtuserpass, mtbotnick, channel, player_z
-from triptera_pe_tests import test_eval, test_string, test_minetest, test_eval_phi, test_string_in
+from triptera_pe_tests import test_eval, test_string, test_minetest, test_eval_phi, test_string_in, mock_create, mock_send_building, test_building_with_pattern
 import pref
+from unittest import mock
 
-x = pref.x
-y = pref.y+18
+cx = pref.x
+cy = pref.y + 18
+z = player_z
+width = 9
+height = 9
+x1 = cx - width // 2
+y1 = cy - height // 2
+x2 = x1 + width
+y2 = y1 + height
 
 
 def test_nodes():
@@ -20,10 +28,9 @@ def test_nodes():
     if len(pybotnick) > NICK_MAX_LEN:
         pybotnick = pybotnick[0:NICK_MAX_LEN]
     mc = MinetestConnection.create(ircserver, mtuser, mtuserpass, mtbotnick, channel, pybotnick=pybotnick)
-    z = player_z
     # floor
-    count_0 = 81 - int(mc.compare_nodes(x-4, y-4, z, x+4, y+4, z, wool0))
-    count_1 = 81 - int(mc.compare_nodes(x-4, y-4, z, x+4, y+4, z, wool1))
+    count_0 = 81 - int(mc.compare_nodes(cx-4, cy-4, z, cx+4, cy+4, z, wool0))
+    count_1 = 81 - int(mc.compare_nodes(cx-4, cy-4, z, cx+4, cy+4, z, wool1))
     if count_0 > 45:
         failed("Too many " + wool0 + ". Should be less than 46 but are " + count_0)
     elif count_1 > 45:
@@ -34,6 +41,32 @@ def test_nodes():
         failed("Too few " + wool1 + ". Should be at least 36 but are " + count_1)
     else:
         passed()
+
+
+def building_pattern(player_z):
+    b = {}
+    # position of centre of square
+    wool = r"wool:(white|grey|dark_grey|black|blue|cyan|green|dark_green|yellow|orange|brown|red|pink|magenta|violet)"
+    b.update(nodebuilder.build(range(x1,x2), range(y1,y2), z, wool))
+    return b
+
+
+def test_building():
+    from task import mc
+    tbwp = test_building_with_pattern(mc, building_pattern)
+    if not tbwp:
+        return False
+    building_guess = mc.building
+    for y in range(y1, y1 + height):
+        prev_xyz = (x1, y, z)
+        prev = building_guess[prev_xyz]
+        for x in range(x1 + 1, x1 + width):
+            xyz = (x, y, z)
+            if prev == building_guess[xyz]:
+                failed("Blocks at " + str(xyz) + " and " + str(prev_xyz) + " are both " + str(prev))
+                return False
+            prev = building_guess[xyz]
+            prev_xyz = (x, y, z)
 
 
 def test_answer_placeholders():
@@ -63,9 +96,15 @@ def test_answer_placeholders():
     return True
 
 
-if __name__ == '__main__':
+@mock.patch('ircbuilder.MinetestConnection.create', mock_create)
+@mock.patch('ircbuilder.MinetestConnection.send_building', mock_send_building)
+def run_patched_tests():
     if test_answer_placeholders():
         run_common_tests()
-        test_nodes()
-        test_minetest(6)
+        test_building()
+        #test_nodes()
+        #test_minetest(6)
 
+
+if __name__ == '__main__':
+    run_patched_tests()
